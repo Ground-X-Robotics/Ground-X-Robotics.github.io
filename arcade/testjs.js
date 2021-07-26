2cce758391e3cbfc3c9263107239793130762c2f
Ground X <61941604+Ground-X-Robotics@users.noreply.github.com>
2021-07-26 03:06:08 +0100

    Update testjs.js

--- arcade/testjs.js
+++ arcade/testjs.js
@@ -1,4 +1,6 @@
 function csAlert(jsString)
 {
-	alert('Unity says:"' + jsString + '"');
+	//alert('Unity says:"' + jsString + '"');
+    if(jsString=="Cube")
+	document.getElementById("colour").style["border"]="1px solid #FF0000;";
 }
