c212b90b55a5eef380a16b351e5d8d145bdb9dfd
Ground X <61941604+Ground-X-Robotics@users.noreply.github.com>
2021-07-26 03:56:57 +0100

    Update interface.js

--- arcade/scripts/interface.js
+++ arcade/scripts/interface.js
@@ -1,1 +1,10 @@
+function csAlert(jsString)
+{
+	alert('Unity says:"' + jsString + '"');
+}
 
+function testSetColour(jsString)
+{
+	console.log(jsString);
+  document.getElementById('colour').style.color=jsString;
+}
